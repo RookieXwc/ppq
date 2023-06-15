74ada68a4709731935cba28a0542a2b2946c85ab
xuweichen <xuweichen1999@126.com>
2023-06-15 12:24:41 +0800

    demo(yolov5): support official yolov5 demo

---
+++ demo/02_Quantization.py
@@ -0,0 +1,55 @@
+# 这里我们展示两种不同的方法去生成 TensorRT Engine
+
+# Plan B: PPQ 导出 engine
+
+import os
+import torchvision.transforms as transforms
+from PIL import Image
+from ppq import *
+from ppq.api import *
+from ppq.utils.TensorRTUtil import build_engine
+
+ONNX_PATH        = 'models/yolov5s.v5.onnx'       # 你的模型位置
+ENGINE_PATH      = 'outputs/yolov5s.v5(ppq).onnx'  # 生成的 Engine 位置
+CALIBRATION_PATH = 'imgs'                         # 校准数据集
+BATCHSIZE        = 1
+EXECUTING_DEVICE = 'cuda'
+# create dataloader
+imgs = []
+trans = transforms.Compose([
+    transforms.Resize([640, 640]),  # [h,w]
+    transforms.ToTensor(),
+])
+for file in os.listdir(path=CALIBRATION_PATH):
+    path = os.path.join(CALIBRATION_PATH, file)
+    img = Image.open(path).convert('RGB')
+    img = trans(img)
+    imgs.append(img) # img is 0 - 1
+
+from torch.utils.data import DataLoader
+dataloader = DataLoader(dataset=imgs, batch_size=BATCHSIZE)
+
+with ENABLE_CUDA_KERNEL():
+    qir = quantize_onnx_model(
+        platform=TargetPlatform.TRT_INT8,
+        onnx_import_file=ONNX_PATH, 
+        calib_dataloader=dataloader, 
+        calib_steps=32, device=EXECUTING_DEVICE,
+        input_shape=[BATCHSIZE, 3, 640, 640], 
+        collate_fn=lambda x: x.to(EXECUTING_DEVICE))
+
+    snr_report = graphwise_error_analyse(
+        graph=qir, running_device=EXECUTING_DEVICE, 
+        dataloader=dataloader, collate_fn=lambda x: x.to(EXECUTING_DEVICE))
+
+    snr_report = layerwise_error_analyse(
+        graph=qir, running_device=EXECUTING_DEVICE, 
+        dataloader=dataloader, collate_fn=lambda x: x.to(EXECUTING_DEVICE))
+
+    export_ppq_graph(
+        graph=qir, platform=TargetPlatform.TRT_INT8,
+        graph_save_to=ENGINE_PATH,
+        config_save_to='outputs/quant_cfg.json')
+
+    build_engine(onnx_file='outputs/yolov5s.v5(ppq).onnx', int8_scale_file='outputs/quant_cfg.json', engine_file='outputs/yolov5s.v5(ppq).engine',
+                 int8=True)
